d4e02469bf4d475ce83e18e8e444cc6282bce5dd
Nguyễn Uy Định <nguyenuydinh@MacBook-Pro-cua-Nguyen.local>
2022-10-11 13:16:02 +0700

    login form use ant design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Form, Input, Button } from 'antd';
 
 function App() {
+  
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <header className='App-header'>
+        <Form 
+          labelCol={{span: 8,}}
+          wrapperCol={{span: 8,}}
+          initialValues={{remember: true}}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          Learn React
-        </a>
+          <Form.Item 
+            name='email'
+            label='email'
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email!',
+              },
+              {
+                type: 'email',
+                message: 'you miss ...@gmail.com'
+              },
+              {
+                validator(_,value) {
+                  if (value.indexOf(' ') === -1)return Promise.resolve() 
+                    else return Promise.reject('You much delete space')
+                }
+              },
+              {
+                validator(_,value) {
+                  if (!/[!#$%^&*()]/.test(value)) return Promise.resolve()
+                    else return Promise.reject('co ky tu dac biet')
+                }
+              },
+            ]}
+            hasFeedback
+          >
+            <Input placeholder='Email' />            
+          </Form.Item>
+
+          <Form.Item 
+            name='password'
+            label='pasword'
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input.Password placeholder='Password'/>
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </header>
     </div>
   );
